66eee9238cff28edcd05d76941f657ad94985f66
ropalma <ropalma@gmail.com>
2015-07-17 01:19:52 -0300

    créditos

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,9 @@ router.get('/', function(req, res, next) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', function(req, res) {
+  res.render('author', {});
+});
+
 
 module.exports = router;
